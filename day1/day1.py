34a258c2940eef4c95be18963a2ee45c26d4569d
Sven Patrick Meier <sven.patrick.meier@gmail.com>
2020-12-02 20:37:25 +0100

    Add solution for second star

--- day1/day1.py
+++ day1/day1.py
@@ -2,13 +2,21 @@ input_file_path = "./input.txt"
 with open(input_file_path, "r") as input:
     all_entries = list(map(lambda x: int(x), input.readlines()))
     input.close()
-print(all_entries)
 
 result = 0
 
+# Task for star one
 for tup_0 in all_entries:
     for tup_1 in all_entries:
         if tup_0 + tup_1 == 2020:
             result = tup_0 * tup_1
             break
+
+# Task for two stars
+for tup_0 in all_entries:
+    for tup_1 in all_entries:
+        for tup_2 in all_entries:
+            if tup_0 + tup_1 + tup_2 == 2020:
+                result = tup_0 * tup_1 * tup_2
+                break
 print(result)
